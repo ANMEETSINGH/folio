f6fefc18fa08dc3621283f1357220ead4acde8e4
ANMEETSINGH <stajinderpal794@gmail.com>
2025-06-26 22:42:10 +0530

    six

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,7 +1,185 @@
 import React from "react";
+import { FiExternalLink } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa6";
 import "./Work.css";
 const Work = () => {
-  return <div></div>;
+  return (
+    <div className="work">
+      <div className="titles">
+        <p>MY WORK</p>
+        <h1>Featured Projects</h1>
+      </div>
+      <div className="cardes">
+        <div className="car1 care">
+          <h3>Typu</h3>
+          <p className="para">
+            A simple tool to generate TypeScript types from
+            <br /> cURL commands.
+          </p>
+          <div className="techno">
+            <button className="t1">react</button>
+            <button className="t1">tailwindcss</button>
+            <button className="t1">typescript</button>
+          </div>
+          <div className="connect">
+            <button className="one">
+              <FiExternalLink /> visit
+            </button>
+            <button className="two">
+              <FaGithub /> Code
+            </button>
+          </div>
+        </div>
+        {/* 2 */}
+        <div className="car1 car2">
+          <h3>Typu2</h3>
+          <p className="para">
+            A simple tool to generate TypeScript types from
+            <br /> cURL commands.
+          </p>
+          <div className="techno">
+            <button className="t1">react</button>
+            <button className="t1">tailwindcss</button>
+            <button className="t1">typescript</button>
+          </div>
+          <div className="connect">
+            <button className="one">
+              <FiExternalLink /> visit
+            </button>
+            <button className="two">
+              <FaGithub /> Code
+            </button>
+          </div>
+        </div>
+        {/* 3 */}
+        <div className="car1 car3">
+          <h3>Typu3</h3>
+          <p className="para">
+            A simple tool to generate TypeScript types from
+            <br /> cURL commands.
+          </p>
+          <div className="techno">
+            <button className="t1">react</button>
+            <button className="t1">tailwindcss</button>
+            <button className="t1">typescript</button>
+          </div>
+          <div className="connect">
+            <button className="one">
+              <FiExternalLink /> visit
+            </button>
+            <button className="two">
+              <FaGithub /> Code
+            </button>
+          </div>
+        </div>
+        {/* 4 */}
+        <div className="car1 car4">
+          <h3>Typu4</h3>
+          <p className="para">
+            A simple tool to generate TypeScript types from
+            <br /> cURL commands.
+          </p>
+          <div className="techno">
+            <button className="t1">react</button>
+            <button className="t1">tailwindcss</button>
+            <button className="t1">typescript</button>
+          </div>
+          <div className="connect">
+            <button className="one">
+              <FiExternalLink /> visit
+            </button>
+            <button className="two">
+              <FaGithub /> Code
+            </button>
+          </div>
+        </div>
+        {/* 5 */}
+        <div className="car1 car5">
+          <h3>Typu5</h3>
+          <p className="para">
+            A simple tool to generate TypeScript types from
+            <br /> cURL commands.
+          </p>
+          <div className="techno">
+            <button className="t1">react</button>
+            <button className="t1">tailwindcss</button>
+            <button className="t1">typescript</button>
+          </div>
+          <div className="connect">
+            <button className="one">
+              <FiExternalLink /> visit
+            </button>
+            <button className="two">
+              <FaGithub /> Code
+            </button>
+          </div>
+        </div>
+        {/* 6 */}
+        <div className="car1 car6">
+          <h3>Typu6</h3>
+          <p className="para">
+            A simple tool to generate TypeScript types from
+            <br /> cURL commands.
+          </p>
+          <div className="techno">
+            <button className="t1">react</button>
+            <button className="t1">tailwindcss</button>
+            <button className="t1">typescript</button>
+          </div>
+          <div className="connect">
+            <button className="one">
+              <FiExternalLink /> visit
+            </button>
+            <button className="two">
+              <FaGithub /> Code
+            </button>
+          </div>
+        </div>
+        {/* 7 */}
+        <div className="car1 car7">
+          <h3>Typu7</h3>
+          <p className="para">
+            A simple tool to generate TypeScript types from
+            <br /> cURL commands.
+          </p>
+          <div className="techno">
+            <button className="t1">react</button>
+            <button className="t1">tailwindcss</button>
+            <button className="t1">typescript</button>
+          </div>
+          <div className="connect">
+            <button className="one">
+              <FiExternalLink /> visit
+            </button>
+            <button className="two">
+              <FaGithub /> Code
+            </button>
+          </div>
+        </div>
+        {/* 8 */}
+        <div className="car1 car8">
+          <h3>Typu</h3>
+          <p className="para">
+            A simple tool to generate TypeScript types from
+            <br /> cURL commands.
+          </p>
+          <div className="techno">
+            <button className="t1">react</button>
+            <button className="t1">tailwindcss</button>
+            <button className="t1">typescript</button>
+          </div>
+          <div className="connect">
+            <button className="one">
+              <FiExternalLink /> visit
+            </button>
+            <button className="two">
+              <FaGithub /> Code
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
