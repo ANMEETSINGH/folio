f9ddf58d6a8f50ed3b3f8cab01e3c6f8ce5c5ffc
ANMEETSINGH <stajinderpal794@gmail.com>
2025-06-26 12:59:06 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Skills.css";
+const Skills = () => {
+  return <div></div>;
+};
+
+export default Skills;
