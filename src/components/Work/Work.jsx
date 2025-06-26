f3f72916616e15b60398f996d2701b58c031307a
ANMEETSINGH <stajinderpal794@gmail.com>
2025-06-26 23:00:01 +0530

    seven

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -23,16 +23,20 @@ const Work = () => {
           </div>
           <div className="connect">
             <button className="one">
-              <FiExternalLink /> visit
+              <a href="https://typu.vercel.app/" target="_blank">
+                <FiExternalLink /> visit
+              </a>
             </button>
             <button className="two">
-              <FaGithub /> Code
+              <a href="https://github.com/thisisyashgarg/typu" target="_blank">
+                <FaGithub /> Code
+              </a>
             </button>
           </div>
         </div>
         {/* 2 */}
         <div className="car1 car2">
-          <h3>Typu2</h3>
+          <h3>UnQueue</h3>
           <p className="para">
             A simple tool to generate TypeScript types from
             <br /> cURL commands.
@@ -44,16 +48,23 @@ const Work = () => {
           </div>
           <div className="connect">
             <button className="one">
-              <FiExternalLink /> visit
+              <a href="https://unqueue.netlify.app/" target="_blank">
+                <FiExternalLink /> visit
+              </a>
             </button>
             <button className="two">
-              <FaGithub /> Code
+              <a
+                href="https://github.com/thisisyashgarg/unqueue"
+                target="_blank"
+              >
+                <FaGithub /> Code
+              </a>
             </button>
           </div>
         </div>
         {/* 3 */}
         <div className="car1 car3">
-          <h3>Typu3</h3>
+          <h3>Foodify</h3>
           <p className="para">
             A simple tool to generate TypeScript types from
             <br /> cURL commands.
@@ -65,16 +76,23 @@ const Work = () => {
           </div>
           <div className="connect">
             <button className="one">
-              <FiExternalLink /> visit
+              <a href="https://foodifyme.netlify.app/" target="_blank">
+                <FiExternalLink /> visit
+              </a>
             </button>
             <button className="two">
-              <FaGithub /> Code
+              <a
+                href="https://github.com/thisisyashgarg/namaste-react"
+                target="_blank"
+              >
+                <FaGithub /> Code
+              </a>
             </button>
           </div>
         </div>
         {/* 4 */}
         <div className="car1 car4">
-          <h3>Typu4</h3>
+          <h3>Ain't Youtube</h3>
           <p className="para">
             A simple tool to generate TypeScript types from
             <br /> cURL commands.
@@ -86,16 +104,23 @@ const Work = () => {
           </div>
           <div className="connect">
             <button className="one">
-              <FiExternalLink /> visit
+              <a href="https://aintyoutube.netlify.app/" target="_blank">
+                <FiExternalLink /> visit
+              </a>
             </button>
             <button className="two">
-              <FaGithub /> Code
+              <a
+                href="https://github.com/thisisyashgarg/aint-youtube"
+                target="_blank"
+              >
+                <FaGithub /> Code
+              </a>
             </button>
           </div>
         </div>
         {/* 5 */}
         <div className="car1 car5">
-          <h3>Typu5</h3>
+          <h3>Cryptologer</h3>
           <p className="para">
             A simple tool to generate TypeScript types from
             <br /> cURL commands.
@@ -107,16 +132,26 @@ const Work = () => {
           </div>
           <div className="connect">
             <button className="one">
-              <FiExternalLink /> visit
+              <a
+                href="https://x.com/IAmCryptologer?t=Rv_xgKBcUDbNc1lbUrBjhw&s=09"
+                target="_blank"
+              >
+                <FiExternalLink /> visit
+              </a>
             </button>
             <button className="two">
-              <FaGithub /> Code
+              <a
+                href="https://github.com/thisisyashgarg/news-tweeter-bot"
+                target="_blank"
+              >
+                <FaGithub /> Code
+              </a>
             </button>
           </div>
         </div>
         {/* 6 */}
         <div className="car1 car6">
-          <h3>Typu6</h3>
+          <h3>Book Management API</h3>
           <p className="para">
             A simple tool to generate TypeScript types from
             <br /> cURL commands.
@@ -128,16 +163,26 @@ const Work = () => {
           </div>
           <div className="connect">
             <button className="one">
-              <FiExternalLink /> visit
+              <a
+                href="https://github.com/thisisyashgarg/vocally-assignment"
+                target="_blank"
+              >
+                <FiExternalLink /> visit
+              </a>
             </button>
             <button className="two">
-              <FaGithub /> Code
+              <a
+                href="https://github.com/thisisyashgarg/vocally-assignment"
+                target="_blank"
+              >
+                <FaGithub /> Code
+              </a>
             </button>
           </div>
         </div>
         {/* 7 */}
         <div className="car1 car7">
-          <h3>Typu7</h3>
+          <h3>Meme Generator</h3>
           <p className="para">
             A simple tool to generate TypeScript types from
             <br /> cURL commands.
@@ -149,16 +194,23 @@ const Work = () => {
           </div>
           <div className="connect">
             <button className="one">
-              <FiExternalLink /> visit
+              <a href="https://creatememes.netlify.app/" target="_blank">
+                <FiExternalLink /> visit
+              </a>
             </button>
             <button className="two">
-              <FaGithub /> Code
+              <a
+                href="https://github.com/thisisyashgarg/meme-generator"
+                target="_blank"
+              >
+                <FaGithub /> Code
+              </a>
             </button>
           </div>
         </div>
         {/* 8 */}
         <div className="car1 car8">
-          <h3>Typu</h3>
+          <h3>FooBank</h3>
           <p className="para">
             A simple tool to generate TypeScript types from
             <br /> cURL commands.
@@ -170,10 +222,17 @@ const Work = () => {
           </div>
           <div className="connect">
             <button className="one">
-              <FiExternalLink /> visit
+              <a href="https://foobank.netlify.app/" target="_blank">
+                <FiExternalLink /> visit
+              </a>
             </button>
             <button className="two">
-              <FaGithub /> Code
+              <a
+                href="https://github.com/thisisyashgarg/banking-app"
+                target="_blank"
+              >
+                <FaGithub /> Code
+              </a>
             </button>
           </div>
         </div>
